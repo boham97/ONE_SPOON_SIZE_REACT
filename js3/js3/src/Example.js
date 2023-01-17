16387eb9647dba7247ebd2682190333d8e553bf0
boham97 <kangbosung97@gmail.com>
2023-01-17 23:07:08 +0900

    기본 토대

--- js3/js3/src/Example.js
+++ js3/js3/src/Example.js
@@ -28,6 +28,7 @@ function Example() {
                 if ( o.isMesh ) {
                     //note: for a multi-material mesh, `o.material` may be an array,
                     // in which case you'd need to set `.map` on each value.
+                    // 텍스쳐 요소들 넣기
                     o.material.map = textureBaseColor;
                     o.material.normalmap = textureNormal;
                     o.material.displacementMap = textureHeight
@@ -38,15 +39,17 @@ function Example() {
 
                 }
             });
-        model.scale.x = 8
-        model.scale.y = 8
-        model.scale.z = 8
+        model.scale.x = 8   //x축으로 8배 확대
+        model.scale.y = 8   //y축으로 8배 확대
+        model.scale.z = 8   //z축으로 8배 확대
 
-        model.rotation.x = 90
+        model.rotation.x = 90 //회전
         scene.add( model );
         function animate2() {
+            // 시간에 따라 회전함
+
             //gltf.scene.rotation.x += 0.1
-            gltf.scene.rotation.z += 0.1
+            //gltf.scene.rotation.z += 0.1
             gltf.scene.rotation.y += 0.1
 
             requestAnimationFrame(animate2)
@@ -54,8 +57,9 @@ function Example() {
         animate2()
     });
 
-    const geo = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+    const geo = new THREE.BoxGeometry(0.5, 0.5, 0.5);   //정육면체
     const mat = new THREE.MeshStandardMaterial({
+        //texture요소 입력
         map : textureBaseColor,
         normalMap : textureNormal,
         displacementMap : textureHeight,
@@ -63,35 +67,35 @@ function Example() {
         roughnessMap : textureRoughness,
         roughness : 1
     })
-    const cube = new THREE.Mesh(geo, mat)
-    cube.position.x = 1
-    scene.add(cube)
+    const cube = new THREE.Mesh(geo, mat)   //오브젝트 생성
+    cube.position.x = 1                     // 생성 위치
+    scene.add(cube)                         //scene에 추가하고 camera에 보여야 렌더링됨
 
-    //console.log(SkeletonUtils.clone)
     const geo2 = new THREE.BoxGeometry(0.5, 0.5, 0.5);
     const mat2 = new THREE.MeshStandardMaterial({
         map : textureBaseColor
+        // texture로 map요소만 넣어서 cube와 다르게 광이나 질감이 없음
     })
     const cube2 = new THREE.Mesh(geo2, mat2)
     cube2.position.x = -1
     scene.add(cube2)
     
     //light
-    const Light1 = new THREE.DirectionalLight(0xffffff, 0.5)
-    Light1.position.set(1, 1, 1)
+    const Light1 = new THREE.DirectionalLight(0xffffff, 0.5)    //백색광, 세기: 0.5
+    Light1.position.set(1, 1, 1)    //ㅇ위치
     scene.add(Light1)
 
 
-    //camera
+    //light2
     const dl = new THREE.DirectionalLight(0xffffff, 0.5)
     scene.add(dl)
 
-    //renderer
+    //renderer 렌더링
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight)
     document.body.appendChild(renderer.domElement);
     
-
+    // cube, cube2가 회전하고 이를 리렌더링함
     function render(time) {
         time *= 0.001
         cube.rotation.x = time
@@ -106,6 +110,7 @@ function Example() {
     }
     requestAnimationFrame(render)
     
+    //반응형 window의 비율이 바뀌면 update
     function onWindow(){
         camera.aspect = window.innerWidth/ window.innerHeight
         camera.updateProjectionMatrix()
--- react/spadiary/src/App.js
+++ react/spadiary/src/App.js
@@ -8,11 +8,33 @@ import Edit from "./pages/Edit";
 
 import RouterLink from "./component/RouterLink";
 
+//component
+import MyButton from "./component/MyButton";
+import MyHeader from "./component/Myheader";
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton 
+              text={'왼쪽 버튼'}
+              onClick={()=>alert('left')} />
+          }
+          rightChild={
+            <MyButton 
+              text={'오른쪽 버튼'}
+              onClick={()=>alert('right')}/>
+          }
+        />
         <h2>App.js</h2>
+        <MyButton
+          text={'버튼'}
+          onClick={() => alert('버튼클릭')}
+          type={"positive"}
+        />
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/new" element={<New/>}/>
