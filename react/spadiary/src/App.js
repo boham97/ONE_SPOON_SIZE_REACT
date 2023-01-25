36251f26c54e04345b20e404de2cc103d1c8758f
boham97 <kangbosung97@gmail.com>
2023-01-25 22:19:39 +0900

    home

--- react/spadiary/src/App.js
+++ react/spadiary/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css"
 import {BrowserRouter, Route, Routes} from "react-router-dom"
+import React, { useRef, useReducer } from "react";
 
 import Home from "./pages/Home";
 import New from "./pages/New";
@@ -12,39 +13,124 @@ import RouterLink from "./component/RouterLink";
 import MyButton from "./component/MyButton";
 import MyHeader from "./component/Myheader";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <MyHeader
-          headText={"App"}
-          leftChild={
-            <MyButton 
-              text={'왼쪽 버튼'}
-              onClick={()=>alert('left')} />
-          }
-          rightChild={
-            <MyButton 
-              text={'오른쪽 버튼'}
-              onClick={()=>alert('right')}/>
-          }
-        />
-        <h2>App.js</h2>
-        <MyButton
-          text={'버튼'}
-          onClick={() => alert('버튼클릭')}
-          type={"positive"}
-        />
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/new" element={<New/>}/>
-          <Route path="/diary/:id" element={<Diary/>}/>
-          <Route path="/edit" element={<Edit/>}/>
-        </Routes>
+const reducer = (state, action) => {
+  let newState = []
+  switch(action.type){
+    case 'INIT':{
+      return action.data
+    }
+    case 'CREATE':{
+      const newItem = {
+        ...action.data
+      }
+      newState = [newItem, ...state]
+      break
+    }
+    case 'REMOVE': {
+      newState = state.filter((it)=>it.id !== action.targetId)
+    }
+    case 'EDIT': {
+      newState = state.map((it)=> it.id === action.data.id ? {...action.data} : it)
+      break
+    }
+    default:
+      return state
+  }
+  return newState
+}
 
-        <RouterLink/>
-      </div>
-    </BrowserRouter>
+export const DiaryStateContext = React.createContext()
+export const DiaryDispatchContext = React.createContext()
+
+const dummyData = [
+  {
+    id: 1,
+    emotion: 1,
+    content: "1번 일기",
+    date: 1674651675117
+  },
+  {
+    id: 2,
+    emotion: 2,
+    content: "2번 일기",
+    date: 1674651675118
+  },
+  {
+    id: 3,
+    emotion: 3,
+    content: "3번 일기",
+    date: 1674651675119
+  },
+  {
+    id: 4,
+    emotion: 4,
+    content: "1번 일기",
+    date: 1674651675120
+  },
+  {
+    id: 5,
+    emotion: 5,
+    content: "2번 일기",
+    date: 1674651675121
+  },
+
+]
+
+function App() {
+  
+  const [data, dispatch] = useReducer(reducer, dummyData)
+
+  const dataId = useRef(0)
+  //create
+  const onCreate = (date,content,emotion)=>{
+    dispatch({
+      type: "CREATE", 
+      data:{
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion
+      }
+    })
+    dataId.current += 1
+  }
+  //remove
+  const onRemove = (targetId)=>{
+    dispatch({type:"REMOVE", targetId})
+  }
+  //edit
+  const onEdit = (targetId,date, content, emotion)=>{
+    dispatch({
+      type:"EDIT",
+      data:{
+        id:targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion
+      }
+    })
+  }
+
+
+
+  return (
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={{
+        onCreate, onRemove, onEdit
+      }}>
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/new" element={<New/>}/>
+              <Route path="/diary/:id" element={<Diary/>}/>
+              <Route path="/edit" element={<Edit/>}/>
+            </Routes>
+            <RouterLink/>
+          </div>
+        </BrowserRouter>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
   );
 }
 
--- react/spadiary/src/pages/Home.js
+++ react/spadiary/src/pages/Home.js
@@ -1,9 +1,60 @@
-const Home = () => {
-    return <div>
-        <h2>home</h2>
-        <p>this is home</p>
+import { useContext, useEffect, useState } from "react";
+import { DiaryStateContext } from "../App";
 
+import MyButton from "../component/MyButton";
+import MyHeader from "../component/Myheader";
+
+const Home = () => {
+	const diaryList = useContext(DiaryStateContext)
+
+
+	const [data,setData] = useState([])
+	const [curDate, setCurDate]= useState(new Date())
+	const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월 ${curDate.getDate()}일`
+  
+	useEffect(()=>{
+		const firstDay = new Date(
+			curDate.getFullYear(),
+			curDate.getMonth(),
+			1
+		).getTime()
+
+		const lastDay = new Date(
+			curDate.getFullYear(),
+			curDate.getMonth() + 1,
+			0
+		).getTime()
+
+		setData(diaryList.filter((it)=> firstDay <= it.date && it.date <= lastDay))
+	},[diaryList, curDate])
+
+	useEffect(()=>{
+		console.log(data)
+	},[data])
+
+	const increaseMonth = () => {
+		setCurDate(
+			new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+		)
+	}
+	
+	const decreaseMonth = () => {
+		setCurDate(
+			new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+		)
+	}
+
+
+	return (
+    <div>
+    	<h2>Home</h2>
+    	<p>this is home</p>    
+			<MyHeader headText={headText}
+				leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
+				rightChild={<MyButton text={">"} onClick={increaseMonth} />}
+			/>
     </div>
+	)
 }
 
 
